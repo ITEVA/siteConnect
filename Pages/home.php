fbf998b7cf6f82da42155627bb4472b58694111e
JuraiAndou <samuel.gomes.afonso@gmail.com>
2015-12-11 17:05:34 -0300

    pagina home html/css/imagens
    Não terminado

--- Pages/home.php
+++ Pages/home.php
@@ -8,52 +8,53 @@
                 A certificação de metodologias que nos auxiliam a lidar com o aumento do diálogo entre os diferentes
                 setores produtivos representa uma abertura para a melhoria do remanejamento dos quadros funcionais.
             </p>
-            <img src="imagens/bg-topo-2pagina.png"/></div>
+            <img src="images/bg-topo-2pagina.png"/></div>
         <div class="slider-item">
             <p>          A certificação de metodologias que nos auxiliam a lidar com a determinação clara de objetivos
                 acarreta um processo de reformulação e modernização dos procedimentos normalmente adotados. Acima de tudo,
                 é fundamental ressaltar que a contínua expansão de nossa atividade assume importantes posições no
                 estabelecimento do orçamento setorial.
             </p>
-            <img src="imagens/bg-topo.png"/></div>
+            <img src="images/bg-topo.png"/></div>
         <div class="slider-item">
             <p>
                 As experiências acumuladas demonstram que a determinação clara de objetivos acarreta um processo de
                 reformulação e modernização das novas proposições.
             </p>
-            <img src="imagens/shutterstock_70728256.jpg" width="100%" height="100%"/></div>
+            <img src="images/shutterstock_70728256.jpg" width="100%" height="100%"/></div>
     </div>
 </div>
 <div class="amarela">
     <div class="acessoRapido">
+        <img src="Images/acesso_rapido.png" id="imgAcessoRapido">
         <div class="botoes">
             <div id="coluna1">
                 <a href="#" class="botao">
-                    <img class="botoes_icon" src="imagens/icon-2ºvia.png"/>
-                    <h3 id="txt1">2ª Via <br/>de Conta</h3>
+                    <img class="botoes_icon" src="images/icon-2ºvia.png"/>
+                    <h3 id="txt1"><span>2ª Via</span><br/>de Conta</h3>
                 </a>
                 <a href="#" class="botao">
-                    <img class="botoes_icon" src="imagens/icon-debit.png"/>
-                    <h3 id="txt2">Débito <br/>Automatico</h3>
+                    <img class="botoes_icon" src="images/icon-debit.png"/>
+                    <h3 id="txt2"><span>Débito</span><br/>Automatico</h3>
                 </a>
                 <a href="#" class="botao">
-                    <img class="botoes_icon" src="imagens/icon-grafic.png"/>
-                    <h3 id="txt3">Controle <br/>de Conta</h3>
+                    <img class="botoes_icon" src="images/icon-grafic.png"/>
+                    <h3 id="txt3"><span>Controle</span><br/>de Conta</h3>
                 </a>
             </div>
 
             <div id="coluna2">
                 <a href="#" class="botao">
-                    <img class="botoes_icon" src="imagens/icon-calendar.png"/>
-                    <h3 id="txt4">Alterar Data <br/>de Vencimento</h3>
+                    <img class="botoes_icon" src="images/icon-calendar.png"/>
+                    <h3 id="txt4"><span>Alterar Data</span><br/>de Vencimento</h3>
                 </a>
                 <a href="#" class="botao">
-                    <img class="botoes_icon" src="imagens/icon-money.png"/>
-                    <h3 id="txt5">Conta <br/>Detalhada</h3>
+                    <img class="botoes_icon" src="images/icon-money.png"/>
+                    <h3 id="txt5"><span>Conta</span><br/>Detalhada</h3>
                 </a>
                 <a href="#" class="botao">
-                    <img class="botoes_icon" src="imagens/icon-archive.png"/>
-                    <h3 id="txt6">Alterar Dados <br/>Cadastrais</h3>
+                    <img class="botoes_icon" src="images/icon-archive.png"/>
+                    <h3 id="txt6">Alterar Dados<br/><span>Cadastrais</span></h3>
                 </a>
             </div>
         </div>
