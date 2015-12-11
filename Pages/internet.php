c3521c0f53a3ad6d23f6b412de72bc17997e375e
JoaoPauloBandeira <joaopaulo.band@hotmai.com>
2015-12-11 15:43:35 -0300

    pagina internet/imagens adcionados

---
+++ Pages/internet.php
@@ -0,0 +1,99 @@
+<div id="Tabelabanner">
+    <img src="images/bg-topo-2pagina.png" id="banner"/>
+    <div class="anuncio">
+        <h4>Ínicio/Internet/Para sua casa</h4>
+        <h2>INTERNET PARA SUA CASA</h2>
+        <p>Diversão, informação e entreterimento 24h por dia em alta velocidade! Acesse a internet sem interferências, 
+        interrupções e, principalmente, sem perda de tempo. Escolha seu plano e Connect sua casa com o mundo.</p>
+    </div>
+    <div class="tabela">    
+        <table border="0" id="tabela">
+            <tr>
+                <td>
+                    <div class="Fundotop"><label for="">R$00,00</label></div>
+                    <img src="images/image-01-mega.png" alt="" class="imgMEGAS">
+                    <button class="Fundobottom"><a href="">CONTRATAR</a></button>
+                </td>
+                <td>
+                    <div class="Fundotop"><label for="">R$00,00</label></div>
+                    <img src="images/image-02-mega.png" alt="" class="imgMEGAS">
+                    <button class="Fundobottom"><a href="">CONTRATAR</a></button>
+                </td>
+                <td>
+                    <div class="Fundotop"><label for="">R$00,00</label></div>
+                    <img src="images/image-04-mega.png" alt="" class="imgMEGAS">
+                    <button class="Fundobottom"><a href="">CONTRATAR</a></button>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <img src="images/Pontilhado.png" alt="" class="pontilhado">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="Fundotop"><label for="">R$00,00</label></div>
+                    <img src="images/image-08-mega.png" alt="" class="imgMEGAS">
+                    <button class="Fundobottom"><a href="">CONTRATAR</a></button>
+                </td>
+                <td>
+                    <div class="Fundotop"><label for="">R$00,00</label></div>
+                    <img src="images/image-10-mega.png" alt="" class="imgMEGAS">
+                    <button class="Fundobottom"><a href="">CONTRATAR</a></button>
+                </td>
+                <td>
+                    <div class="Fundotop"><label for="">R$00,00</label></div>
+                    <img src="images/image-20-mega.png" alt="" class="imgMEGAS">
+                    <button class="Fundobottom"><a href="">CONTRATAR</a></button>
+                </td>
+            </tr>
+        </table>
+        <!--Mini tabela-->
+        <div class="mini_tabela">
+            <div class="coluna_bloco">
+                    <div class="primeiro_bloco">
+                        <img src="images/image-01-mega.png" alt="">
+                        <label for="" class="preco">R$00,00</label>
+                        <button class="btn_contratar">CONTRATAR</button>
+                    </div>
+                    <div class="segundo_bloco">
+                        <img src="images/image-02-mega.png" alt="">
+                        <label for="" class="preco">R$00,00</label>
+                        <button class="btn_contratar">CONTRATAR</button>
+                    </div>
+                    <div class="terceiro_bloco">
+                        <img src="images/image-04-mega.png" alt="">
+                        <label for="" class="preco">R$00,00</label>
+                        <button class="btn_contratar">CONTRATAR</button>
+                    </div>
+            </div>
+            <div class="coluna_bloco">
+                <div class="primeiro_bloco">
+                    <img src="images/image-08-mega.png" alt="">
+                    <label for="" class="preco">R$00,00</label>
+                    <button class="btn_contratar">CONTRATAR</button>
+                </div>
+                <div class="segundo_bloco">
+                    <img src="images/image-10-mega.png" alt="">
+                    <label for="" class="preco">R$00,00</label>
+                    <button class="btn_contratar">CONTRATAR</button>
+                </div>
+                <div class="terceiro_bloco">
+                    <img src="images/image-20-mega.png" alt="">
+                    <label for="" class="preco">R$00,00</label>
+                    <button class="btn_contratar">CONTRATAR</button>
+                </div>
+            </div>
+        </div>
+        <!--Fim Mini Tabela-->
+    </div>        
+</div>
+    <div id="info">
+        <div class="img_info">
+            <div class="bloco_info">
+                <h2 id="h2">Fale com a gente</h2>
+                <h5 id="h5">Entre em contato conosco agora mesmo e conheça melhor as soluções da Connect para você!</h5>
+                <h4 id="h4">0800 000 0000 | (85) 0000-0000</h4>
+            </div> 
+        </div>
+    </div>
