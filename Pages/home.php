44ca72cd0614c29302cd345e977e034d4c656064
JuraiAndou <samuel.gomes.afonso@gmail.com>
2015-12-16 17:12:10 -0300

    pagina home
    tabela da div amarela e div muito mais para você

--- Pages/home.php
+++ Pages/home.php
@@ -28,34 +28,38 @@
 <div id="amarela">
     <div id="amarela-trsp">
         <div id="Cl1_amarela-trsp">
-            <a id="link-item_amarela-trsp1" href="">
+            <a id="link-item_amarela-trsp1" class="link-item_amarela-trsp_up">
                 <div class="item_amarela-trsp">
-                    <img src="" alt=""/>
-                    <h1></h1>
-                    <p></p>
+                    <img src="Images/icon-checked.png"/>
+                    <h1 class="titulo_link-item_amarela-trsp">QUERO<br/>ASSINAR JÁ!</h1>
+                    <p class="conteudo_link-item_amarela-trsp">Preencha o formulário e<br/>entraremos em contato<br/>em menos de 24 horas.</p>
                 </div>
             </a>
-            <a id="link-item_amarela-trsp2" href="">
+            <a id="link-item_amarela-trsp2" class="link-item_amarela-trsp_down">
                 <div class="item_amarela-trsp">
-                    <img src="" alt=""/>
-                    <h1></h1>
-                    <p></p>
+                    <img src="Images/icon-present.png"/>
+                    <h1 class="titulo_link-item_amarela-trsp">OS MELHORES<br/>PLANOS PARA VOCÊ</h1>
+                    <p class="conteudo_link-item_amarela-trsp">Temos planos e preços<br/>que cabem no seu bolso.<br>É só escolher e assinar.</p>
                 </div>
             </a>
         </div>
+        <div id="separador">
+            <img class="imgSeparador" src="Images/LinhaSeparadora.png" width="100%"
+                height="100%"/>
+        </div>
         <div id="Cl2_amarela-trsp">
-            <a id="link-item_amarela-trsp3" href="">
+            <a id="link-item_amarela-trsp3" class="link-item_amarela-trsp_up">
                 <div class="item_amarela-trsp">
-                    <img src="" alt=""/>
-                    <h1></h1>
-                    <p></p>
+                    <img src="Images/icon-local.png"/>
+                    <h1 class="titulo_link-item_amarela-trsp">NOSSA<br/>COBERTURA</h1>
+                    <p class="conteudo_link-item_amarela-trsp">Descubra onde encontrar<br/>internet de qualidade,<br/>veja aonde atendemos.</p>
                 </div>
             </a>
-            <a id="link-item_amarela-trsp4" href="">
+            <a id="link-item_amarela-trsp4" class="link-item_amarela-trsp_down">
                 <div class="item_amarela-trsp">
-                    <img src="" alt=""/>
-                    <h1></h1>
-                    <p></p>
+                    <img src="Images/icon-talk.png"/>
+                    <h1 class="titulo_link-item_amarela-trsp">FALE COM<br/>A GENTE!</h1>
+                    <p class="conteudo_link-item_amarela-trsp">Descubra onde encontrar<br/>internet de qualidade,<br/>veja aonde atendemos.</p>
                 </div>
             </a>
         </div>
@@ -93,6 +97,36 @@
                     <h3 id="txt6">Alterar Dados<br/><span>Cadastrais</span></h3>
                 </a>
             </div>
+        </div>
+    </div>
+</div>
+
+<div id="muitoPVc">
+    <h1 id="h1MuitoPVc">MUITO MAIS PARA VOCÊ!</h1>
+    <div id="opcoes_muitoPVc">
+        <div id="tv">
+            <img src="Images/image-TV.png">
+            <h1>Smart tv's</h1>
+            <p>O melhor do entretenimento<br/>com a qualidade da Connect.</p>
+            <a></a>
+        </div>
+        <div id="game">
+            <img src="Images/image-game.png">
+            <h1>Jogos online</h1>
+            <p>Divirta-se em alta velocidade com os<br/>jogos online mais acessados da web.</p>
+            <a></a>
+        </div>
+        <div id="dispositivos">
+            <img src="Images/image-dispositivos.png"/>
+            <h1></h1>
+            <p></p>
+            <a></a>
+        </div>
+        <div id="musicas">
+            <img src="Images/image-music.png">
+            <h1></h1>
+            <p></p>
+            <a></a>
         </div>
     </div>
 </div>
