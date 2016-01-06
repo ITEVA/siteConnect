063f650059fc3bdebb81e106c46f51d31e9af9d1
Giselly Rebouças <gisellyazevedo@hotmail.com>
2016-01-06 16:54:12 -0300

    A Empresa 06012016

--- Pages/aempresa.php
+++ Pages/aempresa.php
@@ -1,3 +1,31 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Documento sem título</title>
+    <link rel="stylesheet" href="Css/aempresa.css">
 
-echo "Ruan nicolas";
+</head>
+
+<body>
+
+    <img class="imagemPrincipal" src="Images/empresa.jpg">
+    <img class="imagemPrincipal2" src="Images/empresa2.jpg">
+    
+    <div class="col-lg-12 div1">
+        <div id="textos">
+            <p id="t1">Início/A Empresa</p>
+            <p id="t2">SOBRE A CONNECT</p>
+            <p align="justify">A Connect surgiu há mais de 10 anos oferecendo inicialmente internet residencial aos seus clientes no bairro Luciano Cavalcante, região metropolitana de Fortaleza. Durante todo este tempo, a Connect vem investindo maciçamente em tecnologia, qualificação de pessoal, expansão de sua rede e sustentabilidade. De um provedor de bairro, a Connect hoje é uma empresa sólida que abrange mais de 15 bairros nesta região, inclusive em fibra óptica, atendendo também clientes coorporativos, possuindo uma equipe técnica especializada e voltada para estar sempre à disposição do mais importante cliente, você!
+
+            Preocupada com o nosso meio ambiente, a Connect investiu em energia renovável, onde o cliente navega de forma limpa e verde, sabendo que toda a energia que está sendo utilizada para manter a nossa estrutura está de painéis solares. Além disso, dispomos de toda uma estrutura de energia segura para que possamos sempre prestar o melhor serviço sem interrupção, 24 horas por dia e 7 dias por semana.
+
+            Guiada pela transparência e respeito ao cliente, e com o compromisso de crescimento contínuo e sustentável, a Connect acredita que assim irá acolher as vontades de seus clientes e com isso colabora para uma sociedade melhor..</p>
+        </div>
+    </div>
+    <div class="col-lg-12 div1">
+        <div class="conteudo">
+            <img id="computador" src="Images/empresaComputadores.jpg">
+        </div>
+    </div>
+</body>
