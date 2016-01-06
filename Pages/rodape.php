b15a1f3de3782650e8126322d8c03577d9be3798
Naldo Maranhão <regisousa7@gmail.com>
2016-01-06 16:02:08 -0300

    Rodape

--- Pages/rodape.php
+++ Pages/rodape.php
@@ -1,3 +1,11 @@
+<img id="bg-baixo" src="Images/bg-baixo.png">
+<div class="col-lg-4 boxInformacao">
+    <div id="informacao">
+        <h2 class="contato" style="">Fale com a gente</h2>
+        <p class="contato">Entre em contato conosco agora mesmo e conheça melhor as soluções da Connect para você!</p>
+        <h5 class="contato">0800 000 0000 | (85) 0000-0000</h5>
+    </div>
+</div>
 <footer>
     <nav id="menuRodape">
         <img src="Images/logoRodape.jpg" alt="" />
