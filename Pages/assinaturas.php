a586f36ea0ce70044a293fda4b81684451fa4276
Naldo Maranhão <regisousa7@gmail.com>
2016-01-06 14:38:21 -0300

    Pgassinaturas ok

---
+++ Pages/assinaturas.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Documento sem título</title>
+    <link rel="stylesheet" href="Css/assinaturas.css">
+
+</head>
+
+<body>
+
+<img id="imagemPrincipal" src="Images/bg-topo-2pagina.png">
+<img id="imagemPrincipal2" src="Images/bg-topo-2(1)pagina.png">
+
+<div class="col-lg-12 div1">
+    <div id="textos">
+        <p id="t1">Início/Internet/Para sua casa</p>
+        <p id="t2">INTERNET PARA SUA CASA</p>
+        <p>Diversão, informação e entreterimento 24h por dia em alta velocidade! Acesse a internet sem inteferências,<br>
+        interrupções e, principalmente, sem perda de tempo. Escolha seu plano e Connect sua casa ao mundo.</p>
+    </div>
+</div>
+<div class="col-lg-12 div2">
+    <div class="conteudo">
+        <div class="linha1">
+            <img class="oferta" src="Images/1mega.png">
+                <div class="row">
+                    <div class="col-xs-4 preco"><p class="valor">R$ 00,00</p></div>
+                    <div class="col-xs-4 contrate"><p class="contratar"><a href="#" class="contrato">CONTRATAR</a></p></div>
+                </div>
+        </div>
+
+        <div class="linha2">
+            <img class="oferta" src="Images/2megas.png">
+                <div class="row">
+                    <div class="col-xs-4 preco"><p class="valor">R$ 00,00</p></div>
+                    <div class="col-xs-4 contrate"><p class="contratar"><a href="#" class="contrato">CONTRATAR</a></p></div>
+                 </div>
+        </div>
+
+        <div class="linha3">
+            <img class="oferta" src="Images/4megas.png">
+                <div class="row">
+                    <div class="col-xs-6 preco"><p class="valor">R$ 00,00</p></div>
+                    <div class="col-xs-6 contrate"><p class="contratar"><a href="#" class="contrato">CONTRATAR</a></p></div>
+
+                    <div class="col-xs-8 preco"><p class="valor">R$ 00,00</p></div>
+                    <div class="col-xs-8 contrate"><p class="contratar"><a href="#" class="contrato">CONTRATAR</a></p></div>
+                 </div>
+        </div>
+    </div>
+
+    <br><br><br><br><br><br>
+
+    <img id="divisao" src="images/divisao.png">
+
+    <br>
+    <div class="conteudo conteudo2">
+        <div class="linha1">
+            <img class="oferta" src="Images/8megas.png">
+            <div class="row">
+                <div class="col-xs-4 preco"><p class="valor">R$ 00,00</p></div>
+                <div class="col-xs-4 contrate"><p class="contratar"><a href="#" class="contrato">CONTRATAR</a></p></div>
+            </div>
+        </div>
+
+        <div class="linha2">
+            <img class="oferta" src="Images/10mega.png">
+            <div class="row">
+                <div class="col-xs-4 preco"><p class="valor">R$ 00,00</p></div>
+                <div class="col-xs-4 contrate"><p class="contratar"><a href="#" class="contrato">CONTRATAR</a></p></div>
+            </div>
+        </div>
+
+        <div class="linha3">
+            <img class="oferta" src="Images/20megas.png">
+            <div class="row">
+                <div class="col-xs-6 preco"><p class="valor">R$ 00,00</p></div>
+                <div class="col-xs-6 contrate"><p class="contratar"><a href="#" class="contrato">CONTRATAR</a></p></div>
+
+                <div class="col-xs-8 preco"><p class="valor">R$ 00,00</p></div>
+                <div class="col-xs-8 contrate"><p class="contratar"><a href="#" class="contrato">CONTRATAR</a></p></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-12 div3"></div>
+
+<img id="bg-baixo" src="Images/bg-baixo.png">
+<div class="col-lg-4 boxInformacao">
+    <div id="informacao">
+        <h2 class="contato" style="">Fale com a gente</h2>
+        <p class="contato">Entre em contato conosco agora mesmo e conheça melhor as soluções da Connect para você!</p>
+        <h5 class="contato">0800 000 0000 | (85) 0000-0000</h5>
+    </div>
+</div>
+
+</img>
+
+</body>
+</html>
