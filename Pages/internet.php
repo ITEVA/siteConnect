3eea840a9cc5d303fdec1d36ad6a0a661142fc8e
JoaoPauloBandeira <joaopaulo.band@hotmail.com>
2016-01-11 13:38:13 -0300

    pg_internet
    pagina internet finalizada

--- Pages/internet.php
+++ Pages/internet.php
@@ -1,24 +1,40 @@
 <div class="col-lg-12 div1">
 	<img id="imagemInternet" src="Images/internet.png">	
 	<p id="t1">Ínicio/Internet/Banda Larga</p>
-	<div id="bloco_texto1">
-		<!-- <img id="imagemInternet2" src="Images/internet2.png"> -->
-		<p id="t2">BANDA LARGA</p>
-		<p id="txt1">
-			Dispondo de uma rede de última feração e equipamentos redundantes, a Connect garante uma conexão rápida, segura e com qualidade, sem limites de downloads e uploads para que você aproveite ao máximo a diversão com a família	
-		</p>
-		<button><a href="">Conheça nossos Planos </a></button>
+	<div id="bloco_principal">
+		<div id="bloco_texto1">
+			<div id="bloco_secundario">
+				<div id="textos1">
+					<div id="imagemInternet2">
+						<img src="Images/internet2.png" alt="" id="imagemInternet2">
+					</div>
+					<p id="t2">BANDA LARGA</p>
+					<p id="txt1">
+						Dispondo de uma rede de última feração e equipamentos redundantes, a Connect garante uma conexão rápida, segura e com qualidade, sem limites de downloads e uploads para que você aproveite ao máximo a diversão com a família.	
+						<button id="planos"><a href="">Conheça nossos Planos </a></button>
+					</p>
+				</div>
+			</div>
+		</div>
 	</div>
 	<div id="bloco_texto2">
-		<p id="t3">PARA QUE LINHA TELEFÔNICA SE VOCÊ PODE TER SOMENTE INTERNET?</p>
-		<p id="txt2">
-			Na Connect você não precisa ter linha telefônica para se conectar universo virtual. 
+		<div id="textos2">
+			<p id="t3" align="justify">PARA QUE LINHA TELEFÔNICA SE VOCÊ PODE TER SOMENTE INTERNET?</p>
+			<p id="txt2" align="justify">
+				Na Connect você não precisa ter linha telefônica para se conectar universo virtual. 
 
-			Com isso, você reduzirá seus custos sem abrir mão da qualidade de sua navegação e evitará surpresas com contas no final do mês, pois nosso valor é fixo e já pré-estabelecido.
-		</p>
-		<p id="t4">CONTRATOS VINCULANTES?</p>
-		<p id="txt3">
-			Não!Aqui na Connect nosso cliente está porque gosta e confia em nossos serviços, e fica até quando desejar!A Connect acredita que o cliente é nosso parceiro, e que ele é livre em suas vontades, por isso não cobra multa contratual pelo motivo de desistência do serviço por curto espaço de tempo ou por motivo diverso.
-		</p>
+				Com isso, você reduzirá seus custos sem abrir mão da qualidade de sua navegação e evitará surpresas com contas no final do mês, 
+
+				pois nosso valor é fixo e já pré-estabelecido.
+			</p>
+			<p id="t4" align="justify">CONTRATOS VINCULANTES?</p>
+			<p id="txt3" align="justify">
+				Não! Aqui na Connect nosso cliente está porque gosta e confia em nossos serviços, 
+
+				e fica até quando desejar!A Connect acredita que o cliente é nosso parceiro, 
+
+				e que ele é livre em suas vontades, por isso não cobra multa contratual pelo motivo de desistência do serviço por curto espaço de tempo ou por motivo diverso.
+			</p>
+		</div>
 	</div>
 </div>
