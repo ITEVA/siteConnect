9b3a4467d90797fbd7b65c038553be17894185dc
Naldo Maranhão <regisousa7@gmail.com>
2016-01-13 09:40:58 -0300

    Page Internet Responsiva (y)

--- Pages/internet.php
+++ Pages/internet.php
@@ -1,40 +1,41 @@
+<img id="imagemPrincipal" src="Images/internet.png">	
+<img id="imagemPrincipal2" src="Images/internet2.jpg">
+
 <div class="col-lg-12 div1">
-	<img id="imagemInternet" src="Images/internet.png">	
-	<p id="t1">Ínicio/Internet/Banda Larga</p>
-	<div id="bloco_principal">
-		<div id="bloco_texto1">
-			<div id="bloco_secundario">
-				<div id="textos1">
-					<div id="imagemInternet2">
-						<img src="Images/internet2.png" alt="" id="imagemInternet2">
-					</div>
-					<p id="t2">BANDA LARGA</p>
-					<p id="txt1">
-						Dispondo de uma rede de última feração e equipamentos redundantes, a Connect garante uma conexão rápida, segura e com qualidade, sem limites de downloads e uploads para que você aproveite ao máximo a diversão com a família.	
-						<button id="planos"><a href="">Conheça nossos Planos </a></button>
-					</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div id="bloco_texto2">
-		<div id="textos2">
-			<p id="t3" align="justify">PARA QUE LINHA TELEFÔNICA SE VOCÊ PODE TER SOMENTE INTERNET?</p>
-			<p id="txt2" align="justify">
-				Na Connect você não precisa ter linha telefônica para se conectar universo virtual. 
-
-				Com isso, você reduzirá seus custos sem abrir mão da qualidade de sua navegação e evitará surpresas com contas no final do mês, 
-
-				pois nosso valor é fixo e já pré-estabelecido.
-			</p>
-			<p id="t4" align="justify">CONTRATOS VINCULANTES?</p>
-			<p id="txt3" align="justify">
-				Não! Aqui na Connect nosso cliente está porque gosta e confia em nossos serviços, 
-
-				e fica até quando desejar!A Connect acredita que o cliente é nosso parceiro, 
-
-				e que ele é livre em suas vontades, por isso não cobra multa contratual pelo motivo de desistência do serviço por curto espaço de tempo ou por motivo diverso.
-			</p>
-		</div>
-	</div>
+    <div id="textos">
+        <p id="t1">Início/Internet/Para sua casa</p>
+    </div>
 </div>
+<div class="col-lg-12 bloco1">
+    <div class="col-lg-12 div2">
+        <div id="textos">
+            <p id="titulo">BANDA LARGA</p>
+            <p id="t2">Dispondo de uma rede de última geração e equipamentos redundantes,<br>
+                       a Connect garante uma conexão rápida, segura e com qualidade, sem <br>
+                       limites de downloads e uploads para que você aproveite ao máximo a<br>
+                       diversão com a família.
+            <img id="familia" src="Images/internetFamilia.png">
+            <div id="botao">
+                <button id="planos">Conheça nossos planos</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-12 bloco2">
+            <div id="textos" class="bloco">
+                 <p id="t3">PARA QUE LINHA TELEFÔNICA SE VOCÊ PODE TER SOMENTE INTERNET?</p>
+                <div id="textosLivres">
+                 <p id="t4" align="justify" class="t4">Na Connect você não precisa ter linha telefônica para se conectar no universo virtual. Com isso,
+                    você reduzirá seus custos sem abrir mão da qualidade de sua navegação e evitará surpresas
+                    com contas no final do mês, pois nosso valor é fixo e já pré-estabelecido.</p>
+                </div>
+                 <p id="t3">CONTRATOS VINCULANTES?</p>
+                <div id="textosLivres">
+                 <p id="t5" class="t4">Não! Aqui na Connect nosso cliente está porque gosta e confia em nossos serviços, e fica até
+                            quando desejar! A Connect acredita que o cliente é nosso parceiro, e que ele é livre em suas
+                            vontades, por isso não cobra multa contratual pelo motivo de desistência do serviço por curto
+                            espaço de tempo ou por motivo diverso.</p>
+
+                </div>
+            </div>
+    </div>
