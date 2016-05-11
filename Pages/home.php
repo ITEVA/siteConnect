99a26b78d30ebc37072f8b8de0f9092672090d08
Giselly Rebouças <gisellyazevedo@hotmail.com>
2016-05-11 16:40:37 -0300

    ajustes

--- Pages/home.php
+++ Pages/home.php
@@ -245,7 +245,7 @@
         <h1 id="h1podLheAjud">PODEMOS LHE AJUDAR?</h1>
         <figure id="image_duvidas">
             <img src="Images/image-duvidas.jpg" id="image_duvidas1">
-            <a href="http://www.speedtest.net/pt/">
+            <a href="velocidade">
             	<img src="Images/imgTesteVelocidadeInternet.jpg" id="image_duvidas2">
             </a>
         </figure>
---
+++ Pages/velocidade.php
@@ -0,0 +1,22 @@
+<!-- BEGIN SPEED TEST - DO NOT ALTER BELOW-->
+<link rel="stylesheet" href="Css/velocidade.css">
+<script type="text/javascript" src="http://168.90.4.230/speedtest/swfobject.js?v=2.2"></script>
+	  <div id="mini-demo">
+              Speedtest.net Mini requer pelo menos a versão 8 do Flash . Por favor<a href="http://get.adobe.com/flashplayer/"> atualize!</a></br>
+              Caso seu dispositivo não tenha suporte, sugerimos que você acesse em outro dispositivo.
+	  </div><!--/mini-demo-->
+	<script type="text/javascript">
+	  var flashvars = {
+			upload_extension: "php"
+		};
+		var params = {
+			wmode: "transparent",
+			quality: "high",
+			menu: "false",
+			allowScriptAccess: "always"
+		};
+		var attributes = {};
+		swfobject.embedSWF("http://168.90.4.230/speedtest.swf?v=2.1.8&autoPlay=true", "mini-demo", "350", "200", "9.0.0", "http://168.90.4.230/speedtest/expressInstall.swf?autoPlay=true", flashvars, params, attributes);
+	</script>
+<!-- END SPEED TEST - DO NOT ALTER ABOVE -->
+
